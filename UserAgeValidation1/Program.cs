3eda520ec9b700c9f717041fcd45df653ab3715f
unknown <aboisen@tktk.ee>
2023-05-02 21:04:43 +0300

    add week2 code

---
+++ UserAgeValidation1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//rakendus küsib kasutajal sisestada tema vanus
+// kui kasutaja vanus on väiksem kui 13, siis konsoolis kuvatakse:
+// "you are too young to use instagram"
+//muul juhul
+//konsoolis kuvatakse "welcome to instagram"
+
+Console.WriteLine("enter your age");
+
+int userAge = int32. parse(Console.ReadLine()); //"13"-heap, rohkem ruumi võtaks, kui on lihtsalt 13-stack vähem vahemälu
+if (userAge >= 13)
+{
+    Console.WriteLine("welcome to instagram!");
+}
+else (userAge < 13)
+{
+    Console.WriteLine("you are too young to use instagram.");
+}
---
+++ UserGenderValidation1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Auli!");
+
+//rakendus küsib kasutajalt valida tema sugu(m/f)
+//raskendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"welcome, mr. [kasutaja perekonnnanimi]" / "welcome, ms. [kasutaja perekonnanimi]"
+
+Console.WriteLine("please, select your gender (m/f):");
+
+char userGender =char.Parse(Console.ReadLine());//loeb konsoolist maha andmeid, string=sõne formaadis
+Console.WriteLine("please, enter your last name:");
+string userLastname = Console.ReadLine();
+
+if(userGender == 'm')
+{
+    Console.WriteLine($"welcome, mr. {userLastname}!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine($"welcome, ms. {userLastname}!");
+}
+else
+{
+    Console.WriteLine($"welcome {userLastname}!");
+}
+
